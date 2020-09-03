fc7dbc3649d26faf72f48a4719f8c5dd11cb2adb
BPitts8019 <bradleyj.pitts@gmail.com>
2020-09-02 20:35:34 -0700

    feat: implement LDI, PRN, and HLT op codes

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,57 @@
 
 import sys
 
+### Registers ###
+R0 = 0x00
+R1 = 0x01
+R2 = 0x02
+R3 = 0x03
+R4 = 0x04
+R5 = 0x05
+R6 = 0x06
+R7 = 0x07
+
+### OP-Codes ###
+# ALU
+ADD = 0b10100000
+SUB = 0b10100001
+MUL = 0b10100010
+DIV = 0b10100011
+MOD = 0b10100100
+INC = 0b01100101
+DEC = 0b01100110
+CMP = 0b10100111
+AND = 0b10101000
+NOT = 0b01101001
+OR = 0b10101010
+XOR = 0b10101011
+SHL = 0b10101100
+SHR = 0b10101101
+
+# PC Mutators
+CALL = 0b01010000
+RET = 0b00010001
+INT = 0b01010010
+IRET = 0b00010011
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
+JGT = 0b01010111
+JLT = 0b01011000
+JLE = 0b01011001
+JGE = 0b01011010
+
+# Other
+NOP = 0b00000000
+HLT = 0b00000001
+LDI = 0b10000010
+LD = 0b10000011
+ST = 0b10000100
+PUSH = 0b01000101
+POP = 0b01000110
+PRN = 0b01000111
+PRA = 0b01001000
+
 
 class CPU:
     """Main CPU class."""
@@ -10,7 +61,25 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.reg[7] = 0xF4
         self.pc = 0
+        self.is_running = False
+        self.perform_op = dict()
+        self.perform_op[LDI] = self.ldi
+        self.perform_op[PRN] = self.prn
+        self.perform_op[HLT] = self.hlt
+
+    def ldi(self, *operands):
+        self.reg[operands[0]] = operands[1]
+        self.pc += 3
+
+    def prn(self, *operands):
+        print(self.reg[operands[0]])
+        self.pc += 2
+
+    def hlt(self, *operands):
+        self.is_running = False
+        self.pc += 1
 
     def load(self):
         """Load a program into memory."""
@@ -70,12 +139,16 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        isRunning = True
-        ir = 0
-
-        while isRunning:
-            print("Running...")
-            self.trace()
-            isRunning = False
+        print("Running...")
+        self.is_running = True
+        while self.is_running:
+            instruction_reg = self.ram_read(self.pc)
+            op_a = self.ram_read(self.pc + 1)
+            op_b = self.ram_read(self.pc + 2)
+            # print("--- Before OP ---")
+            # self.trace()
+            self.perform_op[instruction_reg](op_a, op_b)
+            # print("--- After OP ---")
+            # self.trace()
 
         print("Shutting Down...")
